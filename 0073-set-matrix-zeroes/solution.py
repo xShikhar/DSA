ab9bf9a951d33f6f5aa35ee6b4d0384b525986f5
Shikhar Agarwal <shikharagarwal238@gmail.com>
2026-05-30 11:51:54 +0000

    Sync LeetCode submission Runtime - 11 ms (65.64%), Memory - 13.2 MB (64.49%)

---
+++ 0073-set-matrix-zeroes/solution.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def setZeroes(self, matrix):
+
+        row_zero = False
+
+        for r in range(len(matrix)):
+            for c in range(len(matrix[0])):
+            
+                if matrix[r][c] == 0:
+                    matrix[0][c] = 0
+
+                    if r>0:
+                        matrix[r][0] = 0
+                    else:
+                        row_zero = True
+        
+        for r in range(1,len(matrix)):
+            for c in range(1,len(matrix[0])):
+                if matrix[0][c] == 0 or matrix[r][0] == 0:
+                    matrix[r][c] = 0
+            
+        if matrix[0][0] == 0:
+            for r in range(len(matrix)):
+                matrix[r][0] = 0
+        
+        if row_zero:
+            for c in range(len(matrix[0])):
+                matrix[0][c] = 0
+
+        return matrix
+

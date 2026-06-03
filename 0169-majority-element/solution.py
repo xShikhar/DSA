8b5f7b7f3422bbc0a64da066647dcf2bcd2e9d56
Shikhar Agarwal <shikharagarwal238@gmail.com>
2026-06-03 14:47:44 +0000

    Sync LeetCode submission Runtime - 13 ms (23.50%), Memory - 21.1 MB (86.55%)

--- 0169-majority-element/solution.py
+++ 0169-majority-element/solution.py
@@ -1,15 +1,10 @@
-class Solution(object):
-    def majorityElement(self, nums):
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+
+        count = {}
         n = len(nums)
-        nums_set = set(nums)
-        nums_list = list(nums_set)
-        m = len(nums_list)
-        count = list(range(m))
-        x= 0
-        for i in nums_list:
-            count[x] = nums.count(i)
-            x = x + 1
         
-        for i in range(m):
-            if count[i] > n//2:
-                return nums_list[i]
+        for num in nums:
+            count[num] = count.get(num, 0) + 1
+            if count[num] > n // 2:
+                return num

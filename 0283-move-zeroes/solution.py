71c74d40e788e4ab15f66ced706cd7cc92291e75
Shikhar Agarwal <shikharagarwal238@gmail.com>
2026-06-03 11:15:35 +0000

    Sync LeetCode submission Runtime - 8 ms (24.06%), Memory - 20.7 MB (23.65%)

--- 0283-move-zeroes/solution.py
+++ 0283-move-zeroes/solution.py
@@ -3,8 +3,6 @@ class Solution(object):
         l = 0
         for r in range(len(nums)):
             if nums[r] != 0:
-                nums[l] = nums[r]
+                nums[l] , nums[r] = nums[r], nums[l]
                 l += 1
-        
-        for i in range(l, len(nums)): 
-            nums[i] = 0
+

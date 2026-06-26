6f4bda6bd7c4e777320addaa9ea80a59320d9927
Shikhar Agarwal <shikharagarwal238@gmail.com>
2026-06-26 05:07:54 +0000

    Sync LeetCode submission Runtime - 14 ms (23.22%), Memory - 20.4 MB (46.30%)

--- 0724-find-pivot-index/solution.py
+++ 0724-find-pivot-index/solution.py
@@ -1,12 +1,19 @@
-class Solution(object):
-    def pivotIndex(self, nums):
-        total = sum(nums)
-        prefix = 0
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        n = len(nums)
+        prefix = [0] * n
 
-        for i in range(len(nums)):
-            suffix = total - prefix - nums[i]
-            if prefix == suffix:
+        for i in range(1,n):
+            prefix[i] = prefix[i-1] + nums[i-1]
+        
+        suffix = [0] * n
+        
+        for i in range(n-2,-1,-1):
+            suffix[i] = suffix[i+1] + nums[i+1]
+        
+        for i in range(n):
+            if suffix[i] == prefix[i]:
                 return i
-            prefix += nums[i]
-
         return -1
+
+            

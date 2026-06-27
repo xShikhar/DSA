58b1940b0ab500ed445e0c50c9f83c2f84c61f00
Shikhar Agarwal <shikharagarwal238@gmail.com>
2026-06-27 09:26:43 +0000

    Sync LeetCode submission Runtime - 141 ms (65.40%), Memory - 25.7 MB (23.03%)

---
+++ 0240-search-a-2d-matrix-ii/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        
+        n , m = len(matrix) , len(matrix[0])
+        row = 0
+        col = m - 1
+        
+        while row<n and col>=0:
+            if matrix[row][col] == target:
+                return True
+            elif matrix[row][col] < target:
+                row += 1
+            else:
+                col -= 1
+        return False

8588208c306110ec469f167d2d232c6c430cdf68
Shikhar Agarwal <shikharagarwal238@gmail.com>
2026-07-05 07:03:30 +0000

    Sync LeetCode submission Runtime - 40 ms (98.32%), Memory - 22.4 MB (22.63%)

---
+++ 0142-linked-list-cycle-ii/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = head
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+            if slow == fast:
+                while head != slow:
+                    head, slow = head.next, slow.next
+                return head
+        return None

b6e52f10cb706b96231f8c563eeff0dddb393271
Shikhar Agarwal <shikharagarwal238@gmail.com>
2026-07-11 11:33:04 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.1 MB (91.91%)

---
+++ 0020-valid-parentheses/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        pairs = {')': '(', ']': '[', '}': '{'}
+
+        for ch in s:
+            if ch in pairs:
+                if not stack or stack[-1] != pairs[ch]:
+                    return False
+                stack.pop()
+            else:
+                stack.append(ch)
+        
+        return not stack
+

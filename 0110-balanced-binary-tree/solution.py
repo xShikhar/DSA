6b340014e7a83e711f3e0600458931e591f0ab3a
Shikhar Agarwal <shikharagarwal238@gmail.com>
2026-08-15 10:15:16 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20.3 MB (69.94%)

---
+++ 0110-balanced-binary-tree/solution.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+
+        def height(node):
+
+            if node == None:
+                return 0
+            
+            lh = height(node.left)
+            if lh == -1:
+                return -1
+            rh = height(node.right)
+            if rh == -1:
+                return -1
+            if abs(lh - rh) > 1:
+                return -1
+            
+            return max(lh,rh) + 1
+        
+        return height(root) != -1
+
+
